fc0f0a89bbba265f1de3ee6f02816c4d40360a7c
Amir Mahdi Soleimani <mohdis.solimani@gmail.com>
2019-12-26 22:34:42 +0330

    first commit

---
+++ Easy Array/Easy Array/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Easy Array
+//
+//  Created by Amir Mahdi Soleimani on 12/26/19.
+//  Copyright © 2019 Amir Mahdi Soleimani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
